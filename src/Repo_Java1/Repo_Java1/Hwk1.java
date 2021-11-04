734931e2bbd68f37fa1c56166c38cdf0ea63d097
charm3 <wkdstory@yahoo.com>
2021-11-04 00:21:28 -0400

    Repo_Java1.Repo_Java1

---
+++ src/Repo_Java1/Repo_Java1/Hwk1.java
@@ -0,0 +1,59 @@
+package Repo_Java1.Repo_Java1;
+
+public class Hwk1 {
+    /* make 8 variables
+    number of hours in a day
+
+
+     */
+    public static void main(String[] args) {
+        byte hours_a_day = 24;
+        System.out.println("total number of hours a day is " +  hours_a_day);
+    /* maximum number of days in a year
+
+     */
+        short maximum_number_of_days_in_a_year = 366;
+        System.out.println("maximum number of days in a year is " + maximum_number_of_days_in_a_year);
+    /* total number of employees in Altria
+
+     */
+        int number_of_employees_in_Altria = 9000;
+        System.out.println("total number of employees in Altria Corporation is " + number_of_employees_in_Altria);
+    /* population in a country
+
+     */
+       long population_in_China = 1402000000;
+       System.out.println("2021 population in China is " + population_in_China);
+
+    /* Mortgage rate current
+
+     */
+        float mortgage_rate = 2.9f;
+        char percentage = '%';
+        System.out.println("\nMortgage rate is " + mortgage_rate + percentage );
+    /* Balance in a bank account
+
+     */
+        double bank_account_balance = -465.75;
+        System.out.println("Charmaine's bank account balance is $ " + bank_account_balance);
+    /* Does the sun rise from the west
+
+        */
+        boolean sun_from_west = false;
+        System.out.println("Does the sun rise from the west? = " + false);
+    /* Initials of your  name
+
+     */
+        char nameInitials1 = 'L';
+        char nameInitials2 = 'L';
+        char nameInitials3 = 'C';
+
+        System.out.println("Charmaine's name initials is = " +nameInitials1 + nameInitials2 + nameInitials3);
+
+
+
+
+
+
+    }
+}
